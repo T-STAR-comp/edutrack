a8edfeb1de120e077a329f99bc776329a8e642a2
unknown <ticketmalawi@gmail.com>
2025-07-16 15:39:16 +0200

    comm

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'EduTrackMW',
+        short_name: 'EduTrack',
+        description: 'Education Management Web App',
+        theme_color: '#1e40af',
+        background_color: '#f3f4f6',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+          },
+          {
+            src: '/vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          },
+        ],
+      },
+    }),
+  ],
 })
